346d7ef2e998c631aaffc17856a25320591f2ff6
junghongseop <a01082372487@gmail.com>
2026-01-18 22:51:24 +0900

    feat: 아이콘 전용 버튼 컴포넌트 제작

---
+++ src/shared/ui/buttons/icon-button.tsx
@@ -0,0 +1,20 @@
+import { ComponentType, SVGProps } from 'react';
+
+interface IconButtonProps {
+  icon: ComponentType<SVGProps<SVGSVGElement>>;
+  onClick?: () => void;
+}
+
+const IconButton = ({ icon: Icon, onClick }: IconButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="flex align-center justify-center rounded-full cursor-pointer hover:bg-[#d1d5db]"
+    >
+      <Icon width={24} height={24} />
+    </button>
+  );
+};
+
+export default IconButton;
