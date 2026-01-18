98ed878e3f1ee48c56f63cd6184fc5bb35285fb3
junghongseop <a01082372487@gmail.com>
2026-01-18 23:34:40 +0900

    style: width, height 적용

--- src/shared/ui/buttons/icon-button.tsx
+++ src/shared/ui/buttons/icon-button.tsx
@@ -10,7 +10,7 @@ const IconButton = ({ icon: Icon, onClick }: IconButtonProps) => {
     <button
       type="button"
       onClick={onClick}
-      className="flex align-center justify-center rounded-full cursor-pointer hover:bg-[#d1d5db]"
+      className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer hover:bg-[#d1d5db]"
     >
       <Icon width={24} height={24} />
     </button>
