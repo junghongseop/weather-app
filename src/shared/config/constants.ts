8b2cbceb6bdc81defe68b04f54bcd0b3b5653a34
junghongseop <a01082372487@gmail.com>
2026-01-19 22:05:46 +0900

    add: 상수 추가

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,2 +1,5 @@
 export const STORAGE_KEY = 'favorite-locations';
 export const MAX_BOOKMARKS = 6;
+
+export const SERVICE_KEY = process.env.NEXT_PUBLIC_API_KEY!;
+export const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY!;
