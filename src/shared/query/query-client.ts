a719dcd969bd27de13600e0802334045b350d913
junghongseop <a01082372487@gmail.com>
2026-01-19 22:04:57 +0900

    feat: query client

---
+++ src/shared/query/query-client.ts
@@ -0,0 +1,14 @@
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 10,
+      gcTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default queryClient;
