cf4d1937da1ad98f147d7d3c33cf44ab54187786
junghongseop <a01082372487@gmail.com>
2026-01-19 22:20:14 +0900

    fix: 배포후 발생한 문제 해결

--- src/shared/api/weather.ts
+++ src/shared/api/weather.ts
@@ -1,22 +1,25 @@
-import { API_KEY, SERVICE_KEY } from '../config/constants';
+import { SERVICE_KEY } from '../config/constants';
 import { getBaseDateTime } from '../lib';
 import axiosClient from './axios-client';
 
 const getWeather = async (nx: number, ny: number) => {
   const { base_date, base_time } = getBaseDateTime();
 
-  const res = await axiosClient.get(API_KEY, {
-    params: {
-      serviceKey: SERVICE_KEY,
-      pageNo: 1,
-      numOfRows: 1000,
-      dataType: 'JSON',
-      base_date,
-      base_time,
-      nx,
-      ny,
-    },
-  });
+  const res = await axiosClient.get(
+    'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst',
+    {
+      params: {
+        serviceKey: SERVICE_KEY,
+        pageNo: 1,
+        numOfRows: 1000,
+        dataType: 'JSON',
+        base_date,
+        base_time,
+        nx,
+        ny,
+      },
+    }
+  );
 
   return res.data.response.body.items.item;
 };
